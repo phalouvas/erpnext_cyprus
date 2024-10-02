ccaa534f6a46856f99a9fb745396b0908d4037ed
Panayiotis Halouvas <phalouvas@gmail.com>
2024-10-02 05:50:35 +0000

    feat: Add VAT Statement report
    - Added the VAT Statement report to the Erpnext Cyprus module.
    - The report calculates the VAT due on sales and other outputs for a given period.
    - The report includes filters for company, from date, to date, cost center, and Cyprus VAT output account.
    - The report displays the description and amount of the VAT due on sales.
    
    Closes #123

---
+++ erpnext_cyprus/erpnext_cyprus/report/vat_statement/vat_statement.py
@@ -0,0 +1,71 @@
+# Copyright (c) 2024, KAINOTOMO PH LTD and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe import _
+
+def get_filters(filters):
+    company = filters.get("company")
+    from_date = filters.get("from_date")
+    to_date = filters.get("to_date")
+    cost_center = filters.get("cost_center")
+    cyprus_vat_output_account = filters.get("cyprus_vat_output_account")
+    return company, from_date, to_date, cost_center, cyprus_vat_output_account
+
+def get_columns():
+	columns = [
+		{
+			"label": _("Description"),
+			"fieldtype": "Data",
+			"fieldname": "description",
+			"width": 500,
+		},
+		{
+			"label": _("Amount"),
+			"fieldtype": "Currency",
+			"fieldname": "amount",
+			"options": "currency",
+			"width": 100,
+		}
+	]
+     
+	return columns
+
+def get_vat_due_on_sales(company, from_date, to_date, cost_center, cyprus_vat_output_account):
+    conditions = [
+        "company = %s",
+        "posting_date >= %s",
+        "posting_date <= %s",
+        "is_cancelled = 0",
+        "credit > 0",
+        "voucher_type != 'Purchase Invoice'"
+    ]
+    values = [company, from_date, to_date]
+
+    if cost_center:
+        conditions.append("cost_center = %s")
+        values.append(cost_center)
+
+    if cyprus_vat_output_account:
+        conditions.append("account = %s")
+        values.append(cyprus_vat_output_account)
+
+    query = """
+        SELECT SUM(credit) as total_credit
+        FROM `tabGL Entry`
+        WHERE {conditions}
+    """.format(conditions=" AND ".join(conditions))
+
+    result = frappe.db.sql(query, values, as_dict=True)
+    total_credit = result[0].get('total_credit') if result else 0
+    return total_credit
+
+def execute(filters=None):
+    columns = get_columns()
+    company, from_date, to_date, cost_center, cyprus_vat_output_account = get_filters(filters)
+    data = []
+    
+    vat_due_on_sales = get_vat_due_on_sales(company, from_date, to_date, cost_center, cyprus_vat_output_account)
+    row = {"description": "VAT due in the period on sales and other outputs", "amount": vat_due_on_sales}
+    data.append(row)
+    return columns, data
