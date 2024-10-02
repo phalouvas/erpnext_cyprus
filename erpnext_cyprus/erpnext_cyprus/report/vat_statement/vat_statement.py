e211dd356ff7596b6442b96d7e584baaebab754c
Panayiotis Halouvas <phalouvas@gmail.com>
2024-10-02 05:59:45 +0000

    refactor: Refactor get_filters function in vat_statement.py
    Simplify the get_filters function in vat_statement.py by removing unnecessary indentation and aligning variable assignments. This improves code readability and maintainability.
    
    Related to #123

--- erpnext_cyprus/erpnext_cyprus/report/vat_statement/vat_statement.py
+++ erpnext_cyprus/erpnext_cyprus/report/vat_statement/vat_statement.py
@@ -5,12 +5,12 @@ import frappe
 from frappe import _
 
 def get_filters(filters):
-    company = filters.get("company")
-    from_date = filters.get("from_date")
-    to_date = filters.get("to_date")
-    cost_center = filters.get("cost_center")
-    cyprus_vat_output_account = filters.get("cyprus_vat_output_account")
-    return company, from_date, to_date, cost_center, cyprus_vat_output_account
+	company = filters.get("company")
+	from_date = filters.get("from_date")
+	to_date = filters.get("to_date")
+	cost_center = filters.get("cost_center")
+	cyprus_vat_output_account = filters.get("cyprus_vat_output_account")
+	return company, from_date, to_date, cost_center, cyprus_vat_output_account
 
 def get_columns():
 	columns = [
@@ -28,44 +28,82 @@ def get_columns():
 			"width": 100,
 		}
 	]
-     
+	 
 	return columns
 
 def get_vat_due_on_sales(company, from_date, to_date, cost_center, cyprus_vat_output_account):
-    conditions = [
-        "company = %s",
-        "posting_date >= %s",
-        "posting_date <= %s",
-        "is_cancelled = 0",
-        "credit > 0",
-        "voucher_type != 'Purchase Invoice'"
-    ]
-    values = [company, from_date, to_date]
+	conditions = [
+		"company = %s",
+		"posting_date >= %s",
+		"posting_date <= %s",
+		"is_cancelled = 0",
+		"credit > 0",
+		"voucher_type != 'Purchase Invoice'"
+	]
+	values = [company, from_date, to_date]
 
-    if cost_center:
-        conditions.append("cost_center = %s")
-        values.append(cost_center)
+	if cost_center:
+		conditions.append("cost_center = %s")
+		values.append(cost_center)
 
-    if cyprus_vat_output_account:
-        conditions.append("account = %s")
-        values.append(cyprus_vat_output_account)
+	if cyprus_vat_output_account:
+		conditions.append("account = %s")
+		values.append(cyprus_vat_output_account)
 
-    query = """
-        SELECT SUM(credit) as total_credit
-        FROM `tabGL Entry`
-        WHERE {conditions}
-    """.format(conditions=" AND ".join(conditions))
+	query = """
+		SELECT SUM(credit) as total_credit
+		FROM `tabGL Entry`
+		WHERE {conditions}
+	""".format(conditions=" AND ".join(conditions))
 
-    result = frappe.db.sql(query, values, as_dict=True)
-    total_credit = result[0].get('total_credit') if result else 0
-    return total_credit
+	result = frappe.db.sql(query, values, as_dict=True)
+	total_credit = result[0].get('total_credit') if result else 0
+	return total_credit
+
+def get_vat_due_on_acquisitions_eu(company, from_date, to_date, cost_center, cyprus_vat_output_account):
+	conditions = [
+		"company = %s",
+		"posting_date >= %s",
+		"posting_date <= %s",
+		"is_cancelled = 0",
+		"credit > 0",
+		"voucher_type = 'Purchase Invoice'"
+	]
+	values = [company, from_date, to_date]
+
+	if cost_center:
+		conditions.append("cost_center = %s")
+		values.append(cost_center)
+
+	if cyprus_vat_output_account:
+		conditions.append("account = %s")
+		values.append(cyprus_vat_output_account)
+
+	query = """
+		SELECT SUM(credit) as total_credit
+		FROM `tabGL Entry`
+		WHERE {conditions}
+	""".format(conditions=" AND ".join(conditions))
+
+	result = frappe.db.sql(query, values, as_dict=True)
+	total_credit = result[0].get('total_credit') if result else 0
+	return total_credit
 
 def execute(filters=None):
-    columns = get_columns()
-    company, from_date, to_date, cost_center, cyprus_vat_output_account = get_filters(filters)
-    data = []
-    
-    vat_due_on_sales = get_vat_due_on_sales(company, from_date, to_date, cost_center, cyprus_vat_output_account)
-    row = {"description": "VAT due in the period on sales and other outputs", "amount": vat_due_on_sales}
-    data.append(row)
-    return columns, data
+	columns = get_columns()
+	company, from_date, to_date, cost_center, cyprus_vat_output_account = get_filters(filters)
+	data = []
+	
+	vat_due_on_sales = get_vat_due_on_sales(company, from_date, to_date, cost_center, cyprus_vat_output_account)
+	row = {"description": "VAT due in the period on sales and other outputs", "amount": vat_due_on_sales}
+	data.append(row)
+	
+	vat_due_on_acquisitions_eu = get_vat_due_on_acquisitions_eu(company, from_date, to_date, cost_center, cyprus_vat_output_account)
+	row = {"description": "VAT due in the period on the acquisitions from other EU Members States", "amount": vat_due_on_acquisitions_eu}
+	data.append(row)
+
+	total_vat_due = vat_due_on_sales + vat_due_on_acquisitions_eu
+	row = {"description": "Total VAT due", "amount": total_vat_due}
+	data.append(row)
+	
+	return columns, data
